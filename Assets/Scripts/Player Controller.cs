ca48680cc2de8c68556717c87dbc081b1eaab16b
FerventLoathing <sergiva441@gmail.com>
2025-04-27 22:29:33 +0200

    Added new Input System, mostly
    Now using the new unity input system.

--- Assets/Scripts/Player Controller.cs
+++ Assets/Scripts/Player Controller.cs
@@ -1,28 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    Vector2 moveInput;
     [SerializeField] float moveSpeed;
-    float diagonalMoveLimiter = 0.7f;
+    Rigidbody2D rb;
+    Animator myAnimator;
 
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
     }
 
     void Update()
     {
-        float vertical = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
-        float horizontal = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        Walk();
+        FlipSprite();
+    }
 
-        if (vertical != 0 && horizontal != 0)
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+
+    void Walk()
+    {
+        //Vector2 playerVelocity = new Vector2(moveInput.x * moveSpeed, moveInput.y * moveSpeed);
+        Vector2 playerVelocity = moveInput * moveSpeed;
+        rb.velocity = playerVelocity;
+
+        if (Mathf.Abs(moveInput.x) > Mathf.Epsilon | Mathf.Abs(moveInput.y) > Mathf.Epsilon)
         {
-            horizontal = horizontal * diagonalMoveLimiter;
-            vertical = vertical * diagonalMoveLimiter;
+            myAnimator.SetBool("isMoving", true);
         }
+        else
+        {
+            myAnimator.SetBool("isMoving", false);
+        }
+    }
 
-        transform.Translate(horizontal, vertical, 0, Space.World);
+    void FlipSprite()
+    {
+        if (Mathf.Abs(moveInput.x) > Mathf.Epsilon)
+        {
+            //this flips the player sprite on the vertical axis, according to the movement command
+            transform.localScale = new Vector2(Mathf.Sign(moveInput.x), 1f);
+        }
     }
 }
